0b92f3c03a69bea93a66434538e380aa96127bf9
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-11-08 18:33:05 +1000

    Add files via upload
    Bubble sort using Javascript

---
+++ bubbleSort.js
@@ -0,0 +1,15 @@
+
+/*Javascript Bubble sort algorithm. Swaps adjacent elements if they are in the wrong order
+* Time complexity O(n^2)
+*/ 
+
+let bubble = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr;
+}
