bcfe5177ae97aa99db3b5f5341d0eaa3343433ac
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-12-12 13:13:21 +1000

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,6 +3,8 @@
 * Time complexity O(n^2)
 */ 
 
+// let arr = [899, 45, 345, 4, 4589, 132, 64, 8, 90, 411, 202, 34, 1, 7, 26, 111, 11, 3444, 93, 704, 404, 501, 666, 832, 2040, 599, 14, 0, 777];
+
 let bubble = arr => {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
