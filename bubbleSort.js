ef8b6da46810fea8322e3e8ed9100a8ea2f8f94c
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-12-12 13:14:04 +1000

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,9 @@
 
 /*Javascript Bubble sort algorithm. Swaps adjacent elements if they are in the wrong order
 * Time complexity O(n^2)
-*/ 
+*/
 
+// Testing array.
 // let arr = [899, 45, 345, 4, 4589, 132, 64, 8, 90, 411, 202, 34, 1, 7, 26, 111, 11, 3444, 93, 704, 404, 501, 666, 832, 2040, 599, 14, 0, 777];
 
 let bubble = arr => {
