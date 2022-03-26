72ee4b633aeb00d55c4890f3cdc8b6568ae9fff5
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2022-03-26 18:44:59 +1000

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,7 +7,7 @@
 // let arr = [899, 45, 345, 4, 4589, 132, 64, 8, 90, 411, 202, 34, 1, 7, 26, 111, 11, 3444, 93, 704, 404, 501, 666, 832, 2040, 599, 14, 0, 777];
 
 let bubble = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
+    for ( let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
